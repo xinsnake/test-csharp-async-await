8080cd38d6a7377ae3887117f1eb54c1d8039a07
= <=>
2022-04-11 11:15:43 +1000

    Add project files.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var s = new Scooter();
+
+            while (true)
+            {
+                if (!s.IsOnTrip)
+                {
+                    s.StartTrip();
+                    s.StartBatteryLoop();
+                }
+
+                if (s.BatteryLevel < 10)
+                {
+                    Console.WriteLine("Battery less than 10, stopping.");
+                    s.StopTrip();
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AsyncAwait/Scooter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    internal class Scooter
+    {
+        public int BatteryLevel { get; set; }
+
+        public bool IsOnTrip { get; set; }
+
+        public Scooter()
+        {
+            BatteryLevel = 15;
+        }
+
+        public async void StartBatteryLoop()
+        {
+            while (true)
+            {
+                Console.WriteLine("Battery level " + BatteryLevel.ToString());
+                if (IsOnTrip)
+                {
+                    BatteryLevel -= 1;
+                }
+                if (BatteryLevel < 5)
+                {
+                    Console.WriteLine("Battery less than 5, stopping.");
+                    return;
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        public void StartTrip()
+        {
+            IsOnTrip = true;
+        }
+        public void StopTrip()
+        {
+            IsOnTrip = false;
+        }
+    }
+}
